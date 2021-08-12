3d36b38acc0ddd220376c4c7cdfb671e38399dc0
Achim Fritz <achim.fritz@b13.de>
2021-08-12 07:56:10 +0200

    [BUGFIX] coding standards

--- Classes/Hooks/DrawItem.php
+++ Classes/Hooks/DrawItem.php
@@ -10,19 +10,18 @@ namespace B13\Listelements\Hooks;
  * of the License, or any later version.
  */
 
-use TYPO3\CMS\Backend\View\PageLayoutViewDrawItemHookInterface;
 use TYPO3\CMS\Backend\View\PageLayoutView;
+use TYPO3\CMS\Backend\View\PageLayoutViewDrawItemHookInterface;
 
 /**
  * Class/Function to manipulate the rendering of item preview content
- *
  */
 class DrawItem implements PageLayoutViewDrawItemHookInterface
 {
 
     /**
      * @param PageLayoutView $parentObject : The parent object that triggered this hook
-     * @param boolean $drawItem : A switch to tell the parent object, if the item still must be drawn
+     * @param bool $drawItem : A switch to tell the parent object, if the item still must be drawn
      * @param string $headerContent : The content of the item header
      * @param string $itemContent : The content of the item itself
      * @param array $row : The current data row for this item
@@ -39,5 +38,4 @@ class DrawItem implements PageLayoutViewDrawItemHookInterface
             \B13\Listelements\Service\ListService::resolveListitems($row);
         }
     }
-
 }
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,30 +1,31 @@
 <?php
+
 defined('TYPO3_MODE') or die();
 
 /**
  * add fields to tt_content table
  */
 $additionalColumns = [
-	// add field for saving the list of elements for a list or a slider
-	'tx_listelements_list' => [
-		'label' => 'LLL:EXT:listelements/Resources/Private/Language/locallang_db.xlf:tt_content.list',
-		'config' => [
-			'type' => 'inline',
-			'foreign_table' => 'tx_listelements_item',
-			'foreign_field' => 'uid_foreign',
-			'appearance' => [
-                'showSynchronizationLink' => FALSE,
-                'showAllLocalizationLink' => TRUE,
-                'showPossibleLocalizationRecords' => TRUE,
-                'showRemovedLocalizationRecords' => TRUE,
-                'expandSingle' => TRUE,
-                'newRecordLinkAddTitle' => FALSE,
+    // add field for saving the list of elements for a list or a slider
+    'tx_listelements_list' => [
+        'label' => 'LLL:EXT:listelements/Resources/Private/Language/locallang_db.xlf:tt_content.list',
+        'config' => [
+            'type' => 'inline',
+            'foreign_table' => 'tx_listelements_item',
+            'foreign_field' => 'uid_foreign',
+            'appearance' => [
+                'showSynchronizationLink' => false,
+                'showAllLocalizationLink' => true,
+                'showPossibleLocalizationRecords' => true,
+                'showRemovedLocalizationRecords' => true,
+                'expandSingle' => true,
+                'newRecordLinkAddTitle' => false,
                 'newRecordLinkTitle' => 'LLL:EXT:listelements/Resources/Private/Language/locallang_db.xlf:tt_content.list.newRecordLinkAddTitle',
-                'useSortable' => TRUE,
-                'useCombination' => FALSE
-			],
-		],
-	],
+                'useSortable' => true,
+                'useCombination' => false
+            ],
+        ],
+    ],
 ];
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $additionalColumns);
