d1b0a7986d8243f04b7d8500fce54227e0deb814
Daniel Gorges <daniel.gorges@b13.com>
2022-06-21 14:45:02 +0200

    [BUGFIX] Show correct overlay record in workspaces
    Relates to: GDCSD-314

--- Classes/Service/ListService.php
+++ Classes/Service/ListService.php
@@ -10,6 +10,7 @@ namespace B13\Listelements\Service;
  * of the License, or any later version.
  */
 
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
 use TYPO3\CMS\Core\Database\Query\Restriction\HiddenRestriction;
@@ -55,6 +56,15 @@ class ListService
         $row[$returnAs] = $queryBuilder
             ->execute()
             ->fetchAll();
+
+        foreach ($row[$returnAs] as $key => $specificRow) {
+            BackendUtility::workspaceOL('tx_listelements_item', $specificRow);
+            if ($specificRow !== false) {
+                $row[$returnAs][$key] = $specificRow;
+            } else {
+                unset($row[$returnAs][$key]);
+            }
+        }
 
         // count the number of non-hidden list items in case we need this for a backend preview warning, error message etc.
         // saved to $row[$returnAs . '-numberOfVisibleItems']
